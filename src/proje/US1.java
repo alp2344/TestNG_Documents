b4e2b862971971e2f0b7a84592b6468881954af3
alp2344 <aygunalpr@gmail.com>
2023-09-16 21:22:19 +0300

    Initial commit

---
+++ src/proje/US1.java
@@ -0,0 +1,23 @@
+package proje;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class US1 extends BaseDriver {
+
+
+
+    @Test
+    public void Test1(){
+
+        driver.get("https://demo.nopcommerce.com/");
+
+
+
+
+
+
+
+
+    }
+}
